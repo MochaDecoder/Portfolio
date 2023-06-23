57e730b61fcf587f6c8ae50740ed3e52348fa758
zoidee <passakornke@rdpd.aerothai.co.th>
2023-06-24 01:55:43 +0700

    feat(works): add works and work component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Introduction from "./components/introduction";
 import Posts from "./components/posts/posts";
+import Works from "./components/works/works";
 
 export default function Home() {
   return (
     <section className="flex flex-col gap-20">
       <Introduction />
       <Posts />
+      <Works />
     </section>
   )
 }
