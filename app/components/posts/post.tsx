3fedeb1a3baf7b946bdec61466313913da3d8ab7
zoidee <passakornke@rdpd.aerothai.co.th>
2023-06-24 20:04:01 +0700

    fix(blog): add dynamic route

--- app/components/posts/post.tsx
+++ app/components/posts/post.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Post({ post, className = 'rounded-lg' }: { post: { slug: string, title: string, date: string, description: string, tags: string[] }, className: string }) {
   return (
     <article className={`bg-white p-4 ${className}`}>
-      <Link href={`blogs/${post.slug}`}>
+      <Link href={`/blogs/${post.slug}`}>
         <h3 className="text-2xl mb-2 font-medium">{post.title}</h3>
       </Link>
       <span className="text-gray-600 mb-4 block">
